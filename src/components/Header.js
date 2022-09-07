2cc0675aaff02839391dcf6237db48984a255412
Gaurang Thakkar <gaurang.thakkar1997@gmail.com>
2022-09-07 15:23:08 +0530

    Authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Button from './Button'
+import {useNavigate} from 'react-router-dom'
 
-function Header() {
+function Header(props) {
+
+  const navigate = useNavigate();
+
   return (
     <div>
-      Header
-      <Button buttonText="Login"/>
+        Think360 React JS Assignment
+        {props.token?
+        <>
+          <Button action={()=>props.setToken()} buttonText={"Logout"} />
+        </>:
+        <>
+          <Button action={()=>navigate('/login')} buttonText={"Login"} />
+        </>
+        }
     </div>
-    
   )
 }
 
-export default Header
+export default Header;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Button from "../components/Button";
+import {useNavigate} from 'react-router-dom'
 
-function Login() {
+function Login({setToken}) {
+
+  const [user, setUser] = useState({username: '', password: ''})
+  let inputs = [];
+
+  const navigate = useNavigate();
+
+  for(let key in user){
+    inputs.push(
+      <>
+        <label>{key}: </label>
+        <input type="text" value={user[key]} 
+        name={key} placeholder={key} 
+        onChange={(e)=>setUser({...user, [key]: e.target.value})} 
+        />
+        <br/>
+      </>
+    )
+    
+  }
+
+  const loginForm = (e) => {
+    e.preventDefault();
+    if(user['username'] =="think360" && user['password'] == 123456){
+      setToken(user);
+      navigate('/')
+    }
+    else{
+      alert("Invalid Credentials");
+    }
+  }
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      Login Form
+      <form>
+        {inputs}
+        <Button action={loginForm} buttonText={"Login"} />
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
